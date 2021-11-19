beae5e0980fbfb867c23883e53938d7364737387
Antoni <email.to.antoni@gmail.com>
2021-11-19 20:31:21 +0700

    update

---
+++ src/Pages/Greeting/Greeting.tsx
@@ -0,0 +1,24 @@
+import { IoIosArrowForward } from "react-icons/io";
+
+import './Greeting.css';
+
+function Greeting() {
+  return (
+    <div className="greeting">
+      <div className="greeting-container">
+        <div className="text-inner">
+          <p>Dear [Guess Name],</p>
+          <h2>You Are Invited!</h2>
+          <p>Wedding Celebration of</p>
+          <h1>Antoni & Sinthia</h1>
+        </div>
+        <div className="btn-open">
+          <IoIosArrowForward size="18px"/>
+          <span className="text-open">Open Invitation</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Greeting;
