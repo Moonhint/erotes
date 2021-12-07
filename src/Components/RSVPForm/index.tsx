d15db9c88b5c853b0f837ab1f34d5630bc876b54
Antoni <email.to.antoni@gmail.com>
2021-12-07 23:38:46 +0700

    update rsvp form

--- src/Components/RSVPForm/index.tsx
+++ src/Components/RSVPForm/index.tsx
@@ -13,14 +13,20 @@ function RsvpForm() {
         <div className="rsvp-form">
             <h4>RSVP</h4>
             <p>Kami tidak sabar menunggu hari pernikahan kami bersama Bapak/Ibu/Saudara/i, mohon konfirmasi kehadiran Bapak/Ibu/Saudara/i. <br/> Terima kasih.</p>
-            <div className="name">[NAMA]</div>
+            <div className="name">[Nama]</div>
             <form onSubmit={handleSubmit(onSubmit)}>
-                <select {...register("attendence")}>
-                    <option value="single">Hadir</option>
-                    <option value="plus_one">Hadir +1</option>
-                    <option value="no_going">Maaf, tidak bisa</option>
-                </select>
-                <input type="submit" />
+                <div className="form-container">
+                    <div className="selectdiv ">
+                        <label>
+                            <select {...register("attendence")}>
+                                <option selected value=""> Apakah kamu bisa hadir? </option>
+                                <option value="can">Bisa hadir</option>
+                                <option value="cannot">Maaf, tidak bisa</option>
+                            </select>
+                        </label>
+                    </div>
+                    <button type="submit">Kirim</button>
+                </div>
             </form>
         </div>
     );
