923ffca671f91f9bba6d261fa23f6fbc9c78edc8
Antoni <email.to.antoni@gmail.com>
2021-12-09 02:58:28 +0700

    remove error

--- src/Components/ImgGalery/GaleryContainer.tsx
+++ src/Components/ImgGalery/GaleryContainer.tsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react';
 import { imageInGalery } from './images';
-import { CSSTransition } from 'react-transition-group';
 
 function GaleryContainer({ visibleIndex }: { visibleIndex: number } ) {
 
