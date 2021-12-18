ad006ba59215b1622a570aa06750311467476e6c
Antoni <email.to.antoni@gmail.com>
2021-12-18 11:15:00 +0700

    update payment method

--- src/Components/QRInfo/index.tsx
+++ src/Components/QRInfo/index.tsx
@@ -1,25 +1,66 @@
+import { useState } from 'react';
 import './QRInfo.css';
 import gopay from '../../imgs/payment/gopay.png';
 import ovo from '../../imgs/payment/ovo.png';
 import bca from '../../imgs/payment/bca.png';
-// import qrbca from '../../imgs/qrs/bca.jpg';
+import qrbca from '../../imgs/qrs/bca.jpg';
+import qrovo from '../../imgs/qrs/ovo.jpg';
+import qrgopay from '../../imgs/qrs/gopay.jpg';
 // import { IoLogoInstagram } from "react-icons/io";
 
 function QRInfo() {
+
+    const [ selectedBtn, setSelectedButton ] = useState('');
+
+    
+    const renderQrCodes = () => {
+
+        if (selectedBtn === 'gopay'){
+            return (
+                <div className="qr-container">
+                    <img src={qrgopay} alt="" width="100px" height="100px"/>
+                </div>                
+            );
+        }else if (selectedBtn === 'ovo'){
+            return (
+                <div className="qr-container">
+                    <img src={qrovo} alt="" width="100px" height="100px"/>
+                </div>
+            );
+        }else if (selectedBtn === 'bca'){
+            return (
+                <div className="qr-container">
+                    <img src={qrbca} alt="" width="100px" height="100px"/>
+                </div>
+            )
+        }else {
+            return null;
+        }
+    }
+
+    const handleBcaClicked = () => {
+        setSelectedButton('bca');
+    }
+
+    const handleOvoClicked = () => {
+        setSelectedButton('ovo');
+    }
+
+    const handleGopayClicked = () => {
+        setSelectedButton('gopay');
+    }
+
     return (
         <div className="qr-info">
             <h2>Wedding Gift</h2>
             <p>For family and friends who would like to send a gift. We would be glad to receive it. Tap the following buttons to send them to us:</p>
             <div className="btn-container">
-                <button className="btn-bca"><img src={bca} alt="" /></button>
-                <button className="btn-gopay"><img src={gopay} alt="" /></button>
-                <button className="btn-ovo"><img src={ovo} alt="" /></button>
+                <button className="btn-bca" onClick={handleBcaClicked}><img src={bca} alt="" /></button>
+                <button className="btn-gopay" onClick={handleGopayClicked}><img src={gopay} alt="" /></button>
+                <button className="btn-ovo" onClick={handleOvoClicked}><img src={ovo} alt="" /></button>
             </div>
             <div className="gift-link">
-                {/* <div className="qr-container">
-                    <img src={qrbca} alt="" width="100px" height="100px"/>
-                </div> */}
-                {/* <p>BCA </p> */}
+                {renderQrCodes()}
             </div>
         </div>
     );
